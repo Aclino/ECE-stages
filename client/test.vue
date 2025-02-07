6acb67c496c71787d0594c8c51681d4a5f735474
ismaelsaid <saidismael100@gmail.com>
2025-02-07 16:35:04 +0100

    effacer les dossier inutiles

--- client/test.vue
+++
@@ -1,8 +0,0 @@
-<template>
-<h1>oscar</h1>
-</template>
-<script setup>
-
-</script>
-<style>
-</style>
--- projet Antoine/Nodejs/serveur/src/auth/auth.router.js
+++
@@ -1,7 +0,0 @@
-import {Router} from 'express';
-import {logUserInUsingEmailAndPassword} from './auth.middlewares';
-const router = Router();
-
-router.post('/login', logUserInUsingEmailAndPassword);
-
-export default router;
